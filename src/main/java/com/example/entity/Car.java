2435e873504dc9c8ef1e1a48b4c768ac76899b33
maadt <147260993+maadt@users.noreply.github.com>
2024-01-30 21:51:48 +0900

    動作確認でエラー
    http://localhost:8080/detail/5（乗客がいない車両）実行でエラー

--- src/main/java/com/example/entity/Car.java
+++ src/main/java/com/example/entity/Car.java
@@ -1,10 +1,13 @@
 package com.example.entity;
 
+import java.util.List;
+
 public class Car {
 	private int id;
 	private String name;
 	private int passengers;
 	private Driver driver;//xmlの「association property」の値と一致させる
+	private List<Customer> customers;
 	
 	public int getId() {
 		return id;
@@ -29,5 +32,11 @@ public class Car {
     }
     public void setDriver(Driver driver) {
         this.driver = driver;
+    }
+    public List<Customer> getCustomers() {
+        return customers;
+    }
+    public void setCustomers(List<Customer> customers) {
+        this.customers = customers;
     }
 }
