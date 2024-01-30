fa01bb5b0f6d8cb58260a93fccce5a6b9e2e3fc0
maadt <147260993+maadt@users.noreply.github.com>
2024-01-30 18:48:23 +0900

    課題１実装完了

--- src/main/java/com/example/entity/Car.java
+++ src/main/java/com/example/entity/Car.java
@@ -4,6 +4,7 @@ public class Car {
 	private int id;
 	private String name;
 	private int passengers;
+	private Driver driver;//xmlの「association property」の値と一致させる
 	
 	public int getId() {
 		return id;
@@ -23,4 +24,10 @@ public class Car {
 	public void setPassengers(int passengers) {
 		this.passengers = passengers;
 	}
+	public Driver getDriver() {
+        return driver;
+    }
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
 }
